cf5e734861c6bb1ef7a93a8fe8d76baa8e4fcb40
ryubears <ryubears@gmail.com>
2017-09-16 11:56:45 -0500

    Added PrivacyPolicy Activity

--- app/src/main/java/com/yehyunryu/android/mywifi2/ui/PrivacyPolicyActivity.java
+++ app/src/main/java/com/yehyunryu/android/mywifi2/ui/PrivacyPolicyActivity.java
@@ -1,19 +1,29 @@
 package com.yehyunryu.android.mywifi2.ui;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.yehyunryu.android.mywifi2.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PrivacyPolicyActivity extends AppCompatActivity {
+    @BindView(R.id.privacy_link) Button mPrivacyLinkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+
+        ButterKnife.bind(this);
 
         //set appropriate action bar background
         ActionBar actionBar = getSupportActionBar();
@@ -22,5 +32,11 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @OnClick(R.id.privacy_link)
+    public void onPrivacyLinkClick() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/policies/privacy/"));
+        startActivity(intent);
     }
 }
